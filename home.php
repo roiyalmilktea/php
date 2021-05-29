ad18da57c79db3bc94cc31a1f59e6cc10496476e
roiyal <78768427+roiyalmilktea@users.noreply.github.com>
2021-05-29 15:24:25 +0900

    追加

---
+++ home.php
@@ -0,0 +1,52 @@
+<doctype html>
+<html>
+    <img src="home.png"alt="toppage"/>
+    <?php echo '零一会の活動記録';?><br>
+    <head>
+        
+    <body>
+        <p1>
+            2021活動履歴
+        </p1>
+        <lu>
+            ーこれまでの活動記録<br>
+        </lu>
+        <li>
+            自己の探究<br>
+        </li>
+        <li>
+            今後の零一会の方針
+        <li>
+            定例会議<br>
+        </li>
+        <h3>
+            活動内容<br>
+        </h3>
+            <h4>1.学生間で情報学部の行事をサポートし、盛り上げる<br>
+            <?php echo '※情報工の学生と活動することもあるよ';?><br>
+            2.オープンキャンパスの手伝いをする<br>
+            3.OBの方をお招きして勉強会やディスカッションをする<br>
+            <?php echo '※情報工の学生と活動することもあるよ';?><br>
+            4.経常ゼミナールを行う<br>
+            </h4>
+        <?php echo '来月の予定';?><br>
+        <ul>
+        <li>
+            定例会議(オンライン)
+        </li>
+        <li>
+            経常ゼミナール
+        </li>
+        </ul>
+        
+         <?php echo '活動が停止している最中、大きな行事も対面で行う'; ?>   
+         <?php echo '1年生,2年生求む';?>
+         <a href="https://localhost8080/admin.com">次のサイト(仮)</a><br>
+         <?php
+         $text="bl19067.cc.it-horoshima.ac.jp";
+         $res=print $text;
+         echo $res;
+         ?>
+    
+    </body>
+</html>
